653f8e419de145837da9506c89da658435f60909
sdobreff <sdobreff@users.noreply.github.com>
2021-07-15 06:28:01 +0000

    [create-pull-request] automated changes sync

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ab9ef8daaf83f19b958c2f468d9620abaa76bd8f',
+        'name' => 'wp-white-security/wp-2fa',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'freemius/wordpress-sdk' => array(
+            'pretty_version' => '2.4.2',
+            'version' => '2.4.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../freemius/wordpress-sdk',
+            'aliases' => array(),
+            'reference' => '84a9be4717effd7697a217e0d931f48ae0d2ecc6',
+            'dev_requirement' => false,
+        ),
+        'wp-white-security/wp-2fa' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ab9ef8daaf83f19b958c2f468d9620abaa76bd8f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
