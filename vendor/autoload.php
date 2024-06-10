f6e6ef18ff3afba4af9741d0ce879dafcdeb6aaa
sdobreff <sdobreff@gmail.com>
2024-06-10 15:47:27 +0300

    Sync from 2024-06-10 (#98)

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit3274::getLoader();
+return ComposerAutoloaderInit8090::getLoader();
